d1248c6be9536772301d60ca447c2c3fee2a584b
Bouhlel Rochdi <rochdi.bouhlel@hotmail.fr>
2023-06-04 01:02:08 +0100

    fix duplicate delete functions url

--- user/urls.py
+++ user/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('create', views.create_new, name='users_create'),
     path('view/<int:user_id>', views.view, name='users_view'),
     path('edit/<int:user_id>', views.edit, name='users_edit'),
-    path('delete/<int:user_id>', views.delete, name='users_delete'),
     
     path('register/', views.register, name='register'),
     path('login/', views.sign_in, name='login'),
