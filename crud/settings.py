66479685b2f9ce40f604da2ef23fb2fb57e03818
Bouhlel Rochdi <rochdi.bouhlel@hotmail.fr>
2023-06-04 19:20:34 +0100

    config upload files

--- crud/settings.py
+++ crud/settings.py
@@ -139,11 +139,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/', '/uploads/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, '/static/'),
