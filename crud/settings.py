84cc23c958d1ccdba7d014e40749f0e0e8009656
Bouhlel Rochdi <rochdi.bouhlel@hotmail.fr>
2023-06-04 19:18:21 +0100

    config upload files

--- crud/settings.py
+++ crud/settings.py
@@ -139,6 +139,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/', '/uploads/'
+MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, '/static/'),
+#     os.path.join(BASE_DIR, '/uploads/'),
+#     # Add more directories as needed
+# ]
--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -43,3 +45,5 @@ urlpatterns = [
     
     
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
