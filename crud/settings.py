5a653714abb4de0a8b75539adfd7e830aa77fdd5
Bouhlel Rochdi <rochdi.bouhlel@hotmail.fr>
2023-06-05 01:40:57 +0100

    setings update

--- crud/settings.py
+++ crud/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '')
